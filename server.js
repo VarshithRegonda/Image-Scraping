92949096a95aee3eb7e9f0830b4f7ca04851b651
VarshithRegonda <varshith4869@gmail.com>
2020-11-18 19:13:48 +0530

    'fixed'

--- server.js
+++ server.js
@@ -27,33 +27,9 @@ app.get('/', (req, res) => {
 })
 
 app.post('/image/', (req, res) => {
-       
-  
- 
- var obj
 let url= req.body
 
-  
 
-// var requestAsync = function(url) {
-//     return new Promise((resolve, reject) => {
-//         var req = request(url, (err, response, body) => {
-//             if (err) return reject(err, response, body);
-//             resolve(Buffer.from(body));
-//         });
-//     });
-// };
-// var getParallel = async function() {
-//   //transform requests into Promises, await all
-//   try {
-//       var data = await Promise.all(urls.map(requestAsync));
-      
-//   } catch (err) {
-//       console.error(err);
-//   }
-//   console.log(data);
-// }
-// getParallel();
 Promise.all(url).then(resonses => resonses.forEach( resonse => {
   request.get(resonse,function(err, re, body){
     const buffer = new Buffer.from(body);
@@ -67,67 +43,17 @@ Promise.all(url).then(resonses => resonses.forEach( resonse => {
       if (err) {
         
      console.log(err,err.stack)
-      // await console.log(Object.values(data.Labels))
+    //  await console.log(Object.values(data.Labels))
      
       }
-    
-      await console.log(data.Labels)
-  
+      await console.log(data.Labels)  
     })
-    // .promise()
-    //  .then(()=>{ console.log  (requests)})
-    //  .catch(()=>{console.log(err);})
-  
- 
-    
+
   })
-  
-   
   })).then(()=>{return res.json({data:'tag'})})
   .catch((err)=>{return res.json(err)})
 }
 
 )
-
-
-  // .then(() => console.log(obj))
-
-  
-
-  
-  // console.log(t)
-  // const parms =     {
-  //   Image: {
-  //     Bytes: texts,
-  //   },
-  //   MaxLabels: 10,
-  // }
-  // ;rekognition.detectLabels(parms, function (err, data) {
-  //   //  req.setHeader(‘Content-Type’, ‘application/json’)
-  //   if (!err) {
-    
-  //      requests.push((data.Labels));
-  //      console.log(requests)
-
-  //   }
-  
-  
-// })
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen('3003')
